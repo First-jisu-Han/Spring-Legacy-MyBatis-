c4716b7fa72cafb654cf720ed2eef07f3c82a45f
First-jisu-Han <weblite@naver.com>
2022-07-28 18:01:54 +0900

    게시판 Vo->DTO 및 Mapper 구현

--- src/main/java/personal/mybatis/repository/BoardMapper.java
+++ src/main/java/personal/mybatis/repository/BoardMapper.java
@@ -2,6 +2,7 @@ package personal.mybatis.repository;
 
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import personal.mybatis.model.Board;
 
 import java.util.List;
@@ -11,24 +12,29 @@ import java.util.List;
 public interface BoardMapper {
 
     // 게시판 글 추가
-    public int insert(Board board);
+    public int insert(Board board) throws Exception;
 
     // 게시판 글 업데이트
-    public int update(Board board);
+    public int update(Board board) throws Exception;
 
     // 게시판 글 삭제
-    public int delete(long id);
+    public int delete(long id) throws Exception;
 
     // 게시판 글 수
-    public int count();
+    public int count() throws Exception;
 
     // 게시판 전체 내역
-    public List<Board> selectAll();
+    public List<Board> selectAll() throws Exception;
 
     // 특정 글 선택해서 가져오기
-    public Board selectById(long id);
+    public Board selectById(long id) throws Exception;
 
-    // 페이지 나누기 -> 현재페이지: page, :size
-    public List<Board> selectByLimit();
+    // 페이지 나누기 -> 현재페이지: page,  페이지당 개시물의 개수 :size
+    public List<Board> selectByLimit(@Param("page") int page ,
+                                     @Param("size") int size) throws Exception;
+
+    public int increment(@Param("id") long id , @Param("requester") String
+
+
 
 }
